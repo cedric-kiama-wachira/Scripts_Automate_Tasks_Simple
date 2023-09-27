ce672445dd5dfbf06cca5b3954965a7429225f59
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-27 21:05:45 +0400

    A simple shell script than can be scheduled by cronjob to be getting the Date timestamp and Linux Kernel Version of our installtion

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Log the date and time the script was last executed
+date >> /home/timeline/Linux_Revision/5.Using_Scripts_To_Automate_Tasks/script.log
+
+cat /proc/version >> /home/timeline/Linux_Revision/5.Using_Scripts_To_Automate_Tasks/script.log
+
+
